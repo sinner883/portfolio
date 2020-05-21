6baef1fe437ad247955262f859426eec3b18a67b
sinner883 <sinner883@users.noreply.github.com>
2020-05-21 15:42:06 +0100

    added pictures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import css from './img/css.svg';
+import html from './img/html.svg';
+import js from './img/js.svg';
+import github from './img/git.svg';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Max Tech Portfolio</h1>
+      <img src={css} alt='pict'></img>
+      <img src={html} alt='pict'></img>
+      <img src={js} alt='pict'></img>
+      <img src={github} alt='pict'></img>
     </div>
   );
 }
